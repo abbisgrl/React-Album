38d8fef7725e2eb58c55cc3256a40e2b14d71a47
Gyanendra Shah <102413865+abbisgrl@users.noreply.github.com>
2022-07-12 19:29:12 +0530

    second commit

--- src/Album.js
+++ src/Album.js
@@ -6,6 +6,10 @@ import './css/album.css';
 function Album() {
   const [data, setData] = useState([]);
   useEffect(() => {
+    getList();
+  }, []);
+
+  function getList() {
     fetch("https://jsonplaceholder.typicode.com/users/1/albums").then(
       (result) => {
         result.json().then((resp) => {
@@ -13,23 +17,38 @@ function Album() {
         });
       }
     );
-  }, []);
+  }
+  function deleteUser(id) {
+    fetch(`https://jsonplaceholder.typicode.com/users/1/albums/${id}`, {
+      method: 'DELETE'
+    }).then((result) => {
+      result.json().then((resp) => {
+        console.warn(resp);
+        getList();
+      });
+    });
+  }
 
+  function updateUser(id) {
+    alert(id);
+  }
   return (
     <div className="Album">
       {console.log(data)}
       {data.map((item) => (
-        <Card style={{ width: '18rem',marginLeft:'10px',marginTop:'10px' }}>
-        <Card.Body>
-          <Card.Title>{`User Id:- ${item.userId}`}</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">{`Id:- ${item.id}`}</Card.Subtitle>
-          <Card.Text>
-            {`Title :- ${item.title}`}
-          </Card.Text>
-          <Card.Link ><Button variant="danger">DELETE</Button>{' '}</Card.Link>
-          <Card.Link ><Button variant="secondary">UPDATE</Button>{' '}</Card.Link>
-        </Card.Body>
-      </Card>
+        <Card style={{ width: '18rem', marginLeft: '10px', marginTop: '10px' }}>
+          <Card.Body>
+            <Card.Title>{`User Id:- ${item.userId}`}</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">{`Id:- ${item.id}`}</Card.Subtitle>
+            <Card.Text>
+              {`Title :- ${item.title}`}
+            </Card.Text>
+            <Card.Link ><Button variant="danger" onClick={() => deleteUser(item.id)}>DELETE</Button>{' '}</Card.Link>
+            <Card.Link ><Button variant="secondary" onClick={(
+              
+            ) => updateUser(item.id)}>UPDATE</Button>{' '}</Card.Link>
+          </Card.Body>
+        </Card>
       ))}
     </div>
   );
